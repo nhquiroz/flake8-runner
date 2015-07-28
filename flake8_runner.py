55e783f748130540f3c60a24111ea67680f595d1
Nicolás Quiroz <nh.quiroz@gmail.com>
2015-07-28 16:41:20 -0300

    Change string interpolation, use format() method instead

--- flake8_runner.py
+++ flake8_runner.py
@@ -9,13 +9,17 @@ from colorama import Fore, init
 init()
 
 
+# <--- color constants --->
+
+GREEN = Fore.GREEN
+RED = Fore.RED
+
+
 # <--- aux functions --->
 
 def run_flake8_on_file(py_file):
     """
     Runs 'flake8' command on 'py_file' passed as argument.
-    Each time an issue is found, number_of_issues_found (global variable) is
-    increased by one unit.
     flake8_exit_code contains the exit code from calling 'flake8' command with
     'py_file' as argument. If flake8_exit_code is equal to zero, it means there
     are no errors and an 'OK' legend will be printed.
@@ -28,7 +32,7 @@ def run_flake8_on_file(py_file):
         issues_found = 1
         return flake8_exit_code
     else:
-        print(Fore.GREEN + "%r... OK.") % py_file
+        print("{}{!r}{}".format(GREEN, py_file, "... OK."))
 
     return issues_found
 
@@ -56,21 +60,21 @@ def exit_if_list_is_empty(py_list):
     """
 
     if not py_list:
-        print(Fore.RED + "No .py files found. Try another directory.")
+        print("{}{}".format(RED, "No .py files found. Try another directory."))
         sys.exit()
 
 
-def print_result(number_of_issues):
+def print_result(issues):
     """
     Prints the result, after all the files from the current directory
     have been analyzed. If issues are found, shows the quantity.
     """
 
-    if number_of_issues > 0:
-        print(Fore.RED + "\n" + "Finished. " + str(number_of_issues) +
-              " issues found.")
+    if issues > 0:
+        print("{}{}{}".format(RED, "\n", "Finished. ", issues,
+              " issues found."))
     else:
-        print("\n" + "Finished. All scripts have passed flake8!")
+        print("{}{}".format("\n", "Finished. All scripts have passed flake8!"))
 
 
 # <--- beginning of the script --->
@@ -78,22 +82,22 @@ def print_result(number_of_issues):
 def main():
     """
     This is the main function of the script. It initializes the
-    number_of_issues_found global variable to zero, then list all the .py
+    issues_found global variable to zero, then list all the .py
     files from the current directory and finnaly applies the 'flake8'
     command to each.
     """
 
-    number_of_issues_found = 0
+    issues_found = 0
 
     py_list = create_py_list_from_directory()
     exit_if_list_is_empty(py_list)
-    list_size = str(len(py_list))
+    n = len(py_list)
 
-    print(list_size + " files to be analyzed: " + "\n")
+    print("{} {} {}".format(n, "files to be analyzed:", "\n"))
 
     for script in py_list:
-        number_of_issues_found += run_flake8_on_file(script)
+        issues_found += run_flake8_on_file(script)
 
-    print_result(number_of_issues_found)
+    print_result(issues_found)
 
 main()
