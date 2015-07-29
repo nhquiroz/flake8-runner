a14c5aca19d956c4a7f1be20211928bac623345f
Nicolás Quiroz <nh.quiroz@gmail.com>
2015-07-29 02:15:11 -0300

    Add speed improvements, use generator instead of list

--- flake8_runner.py
+++ flake8_runner.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python
 
+"""
+A simple script that runs flake8 on every .py file, from the current directory.
+Nicolas Quiroz
+https://github.com/nhquiroz/flake8-runner
+GPL v2.0 License.
+"""
+
 # <--- modules needed --->
 
-import sys
 import glob
 import subprocess
 from colorama import Fore, init
@@ -36,26 +42,28 @@ def run_flake8_on(py_file):
     return issues
 
 
-def create_py_list_from_directory():
+def create_py_gen_from_directory():
     """
-    Returns a sorted list, created from all the existent .py files on the
+    Returns a generator, to iterate over all the existent .py files on the
     current directory (except for this script).
     """
 
-    py_list = [py for py in glob.glob("*.py") if py != 'flake8_runner.py']
-    py_list.sort()
+    py_gen = (py for py in glob.glob("*.py") if py != 'flake8_runner.py')
 
-    return py_list
+    return py_gen
 
 
-def exit_if_list_is_empty(py_list):
+def exit_if_gen_is_empty(py_gen):
     """
-    If the list passed as argument is empty, this functions prints an error
-    message and stops the execution of the script.
+    If the iterator passed as argument is empty (None), this functions prints
+    an error message and stops the execution of the script.
     """
 
-    if not py_list:
+    try:
+        next(py_gen)
+    except StopIteration:
         print("{0}No '*.py' files found. Try another directory.".format(RED))
+        import sys
         sys.exit()
 
 
@@ -75,21 +83,18 @@ def show_results(issues):
 
 def main():
     """
-    This is the main function of the script. It lists all the .py
-    files from the current directory and then applies the 'flake8'
-    command to each. Finally, prints the results.
+    This is the main function of the script. It iterates over all the .py
+    files from the current directory and applies the 'flake8' command to each.
+    Finally, it prints the results.
     """
 
     issues_found = False
 
-    py_list = create_py_list_from_directory()
-    exit_if_list_is_empty(py_list)
+    py_gen = create_py_gen_from_directory()
+    exit_if_gen_is_empty(py_gen)
 
-    number_of = len(py_list)
-    print("{0} files to be analyzed: \n".format(number_of))
-
-    for script in py_list:
-        issues_found = run_flake8_on(script)
+    for py_file in py_gen:
+        issues_found = run_flake8_on(py_file)
 
     show_results(issues_found)
 
